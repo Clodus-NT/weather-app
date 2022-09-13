79e40b36171b0db8870181160894f3483146fe74
Clodus-NT <clodus.nt@gmail.com>
2022-09-13 09:59:56 -0400

    configured api call for fetching weather data

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import Forecast from './components/Forecast/Forecast';
 import CurrentWeather from './components/CurrentWeather/CurrentWeather';
 import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [weather, setWeather] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
     console.log(searchData);
-    const [lat, lon] = searchData.value.split(" ");
+    const [lat, lon] = searchData.value;
 
-    const CurrentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`)
+    const weatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${WEATHER_API_KEY}`)
+
+    Promise.all([weatherFetch])
+      .then(async (response) => {
+        const weatherReponse = await response[0].json();
+
+        setWeather({city: searchData.label, ...weatherReponse});
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   }
+
+  console.log('weather: ', weather)
 
   return (
     <div className="appContainer">
       <Search onSearchChange={handleOnSearchChange}/>
-      <CurrentWeather />
+      {weather && <CurrentWeather data={weather} />}
       <Forecast />
     </div>
   );
